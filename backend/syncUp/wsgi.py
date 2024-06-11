0a3a76463d9848ae4c58a6fe82158efd9f73673d
Anurag Yadav <yadavanurag1310@gmail.com>
2024-06-11 18:26:29 +0530

    reset the project, make changes in project name, added new app "User" and api folder to store api

--- backend/syncUp/wsgi.py
+++ backend/syncUp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for easyMeet project.
+WSGI config for syncUp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'easyMeet.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'syncUp.settings')
 
 application = get_wsgi_application()
