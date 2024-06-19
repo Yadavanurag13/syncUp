7ef722bdea7378947e28c2aa3c3561379ffd8090
Anurag Yadav <yadavanurag1310@gmail.com>
2024-06-20 01:23:12 +0530

    Update user serializer with validation

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -1,31 +1,27 @@
 from rest_framework import serializers
 from django.contrib.auth.hashers import make_password
-from .models import CustomUser
+from .models import User
 
-class CustomUserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, help_text='Leave empty if not changing')
-    password2 = serializers.CharField(write_only=True, required=True)
+class RegisterUserSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    username = serializers.CharField(max_length=255)
+    first_name = serializers.CharField(max_length=255)
+    last_name = serializers.CharField(max_length=255)
+    password = serializers.CharField(write_only=True)
 
-    class Meta:
-        model = CustomUser
-        fields = ['id', 'email', 'username', 'first_name', 'last_name', 'password', 'password2', 'is_active', 'is_staff', 'is_superuser']
-        extra_kwargs = {
-            'password': {'write_only': True}
-        }
-
-    def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
-        return attrs
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Email is already registered")
+        return value
 
     def create(self, validated_data):
-        validated_data.pop('password2')
-        validated_data['password'] = make_password(validated_data['password'])
-        return super().create(validated_data)
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            password=validated_data['password']
+        )
+        return user
+        return user
 
-    def update(self, instance, validated_data):
-        validated_data.pop('password2', None)
-        password = validated_data.pop('password', None)
-        if password:
-            instance.password = make_password(password)
-        return super().update(instance, validated_data)
